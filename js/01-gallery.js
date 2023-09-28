521f75595ae01b163e6f14aeb64a89be68527b2c
Anton <a.chertok@akatech.com.ua>
2023-09-28 10:50:13 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,24 +1,7 @@
 import { galleryItems } from "./gallery-items.js";
 
 const gallery = document.querySelector(".gallery");
-
-const images = galleryItems
-  .map(
-    ({ preview, original, description }) => `
-<li class="gallery__item">
-  <a class="gallery__link" href="${original}">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    />
-  </a>
-</li>
-`
-  )
-  .join("");
-
+const images = galleryItems.map(createGalleryMarkup).join("");
 gallery.insertAdjacentHTML("afterbegin", images);
 
 gallery.addEventListener("click", (e) => {
@@ -32,32 +15,43 @@ gallery.addEventListener("click", (e) => {
   const url = image.dataset.source;
   const alt = image.alt;
 
-  const modal = basicLightbox.create(
-    `
-    <div class="modal">
-       <img src="${url}" alt="${alt}"/>
-    </div>
-`,
-    {
-      onShow: (modal) => {
-        console.log("show");
-        // toggleListener(modal, true);
-        document.addEventListener("keydown", onModalEscClose);
-      },
-
-      onClose: (modal) => {
-        console.log("close");
-        // toggleListener(modal, false);
-        document.removeEventListener("keydown", onModalEscClose);
-      },
-    }
-  );
-
-  function onModalEscClose(e) {
-    if (e.code === "Escape") {
-      modal.close();
-    }
-  }
-
+  const modal = createImageModal(url, alt);
   modal.show();
 });
+
+function createGalleryMarkup({ preview, original, description }) {
+  return `
+  <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+       class="gallery__image"
+       src="${preview}"
+       data-source="${original}"
+       alt="${description}"
+      />
+    </a>
+  </li>      
+`;
+}
+
+function createImageModal(url, alt) {
+  const markup = `<img src="${url}" alt="${alt}"/>`;
+  const modal = basicLightbox.create(markup, {
+    onShow: () => {
+      document.addEventListener("keydown", onEscCloseBinded);
+    },
+
+    onClose: () => {
+      document.removeEventListener("keydown", onEscCloseBinded);
+    },
+  });
+
+  const onEscClose = function (e) {
+    if (e.code === "Escape") {
+      this.close();
+    }
+  };
+  const onEscCloseBinded = onEscClose.bind(modal);
+
+  return modal;
+}
